f05bd41b591885528541f32703002352085c21a8
szajsjem <dominik.szczypior01@gmail.com>
2023-11-13 10:36:47 +0100

    Spring initializr project

---
+++ src/test/java/com/game/start/GameStart/GameStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.start.GameStart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
