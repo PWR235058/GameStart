d1fabdafbd0f128e37e2671a29af1a95cd110aa6
szajsjem <dominik.szczypior01@gmail.com>
2023-12-11 04:54:25 +0100

    address get

--- src/main/java/com/game/start/GameStart/REST/Profile.java
+++ src/main/java/com/game/start/GameStart/REST/Profile.java
@@ -1,13 +1,13 @@
 package com.game.start.GameStart.REST;
 
 import com.game.start.GameStart.entity.Address;
+import com.game.start.GameStart.entity.Client;
 import com.game.start.GameStart.entity.User;
 import com.game.start.GameStart.jpa.AddressRepository;
+import com.game.start.GameStart.jpa.ClientRepository;
 import com.game.start.GameStart.jpa.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Profile {
@@ -17,15 +17,23 @@ public class Profile {
     Login sessions;
     @Autowired
     private UserRepository users;
+    @Autowired
+    private ClientRepository clients;
 
-    @PutMapping("/api/profile")
+    @PostMapping("/api/profile")
     String editAddres(@RequestBody Address address){
-        User u = sessions.getUser(null);
+        Client u = sessions.getClient();
         if(u==null)return "{\"ok\":false}";
         address.setId(0L);
         address = adres.save(address);
-        u.getClient().setAddress(address);
-        users.save(u);
+        u.setAddress(address);
+        clients.save(u);
         return "{\"ok\":true}";
+    }
+    @GetMapping("/api/profile")
+    Address getAddres(){
+        Client u = sessions.getClient();
+        if(u==null)throw new RuntimeException();
+        return u.getAddress();
     }
 }
